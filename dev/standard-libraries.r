dfa697e6de782cb6c8538b653c8f584bfbecbfc2
David G <dgabbe@acm.org>
2016-11-02 20:59:23 -0400

    Add new function.

--- dev/standard-libraries.r
+++ dev/standard-libraries.r
@@ -1,89 +1,8 @@
-<<<<<<< HEAD
-<<<<<<< .merge_file_TG5eS2
-#' Reinstall my standard collection of packages to first library listed in \code{.libPaths}.
-#'
-#' Each (minor) version of R uses a unique library path, \code{/Library/Frameworks/R.framework/Versions/3.x/Resources/library} to install packages.  To save disk space, keep all major versions of R packages in one directory.  Also handy when setting a new user environment.
-#'
-#' As a programming exercise, created a function to install my common packages using try/catch and apply.
-#'
-#' @section EXPERIMENTAL
-#'
-#' This code does work, but it is not professional quality.  Use at your own discretion.  A local git repo is not handled.
-#'
-#' @param pkgs The list of packages to install.  If there is no param, a default set will be installed.  A Github package is denoted by \code{username/project-name}.
-#'
-#' @param quiet Logical passed to \code{install.package} parameters \code{quiet} and \code{verbose} to minimize the amount of output
-#'
-#' @export
-reinstall_packages <- function (pkgs = NA, quiet = TRUE) {
-    if (is.na(pkgs))
-      pkgs <- c(
-        "crayon",
-        "dgabbe/dgutils",
-        "directlabels",
-        "dplyr",
-        "ggplot2",
-        "knitr",
-        "lubridate",
-        "plotrix",
-        "RColorBrewer",
-        "readr",
-        "readxl",
-        "reshape2",
-        "rmarkdown",
-        "roxygen2",
-        "rsconnect",
-        "rversions",
-        "scales",
-        "shiny",
-        "shinythemes",
-        "stringr",
-        "tidyr"
-      )
-=======
-# tryCatch(
-#   {
-#     find.package(p, quiet = TRUE)
-#   },
-#   error = function(e) install.packages(p, quiet = TRUE),
-#   finally = print(cat("\n  ", p))
-# )
-#
->>>>>>> .merge_file_vhtDYj
-
-||||||| merged common ancestors
-# tryCatch(
-#   {
-#     find.package(p, quiet = TRUE)
-#   },
-#   error = function(e) install.packages(p, quiet = TRUE),
-#   finally = print(cat("\n  ", p))
-# )
-#
-
-=======
->>>>>>> dev
 installed_pkgs <- c() # List of packages already installed
 error_pkgs <- c() # List of packages that were not installed for some reason
 new_pkgs <- c() # List of packages this function installed
 
-<<<<<<< HEAD
-<<<<<<< .merge_file_TG5eS2
-    # Install first so install_github() is available.  Don't attach devtools!
-    tryCatch(
-      {
-        p = "devtools"
-        find.package(p, quiet = TRUE)
-      },
-      error = function(e) install.packages(p, quiet = quiet, verbose = quiet),
-      finally = print(cat("\n  ", p))
-=======
-#' Title
-||||||| merged common ancestors
-#' Title
-=======
 #' reinstall_packages installs your standard set of packages
->>>>>>> dev
 #'
 #' @param pkgs
 #'
@@ -113,7 +32,6 @@ reinstall_packages <- function (pkgs = NA) {
       "shiny",
       "stringr",
       "tidyr"
->>>>>>> .merge_file_vhtDYj
     )
 
   #

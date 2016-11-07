0a2ba39e033a3f90f27c50d537ba095356736f5f
David G <dgabbe@acm.org>
2016-11-07 15:48:58 -0500

    Official release.  All future will take place on a dev branch.
    Updated to .Rbuildignore to get clean package build; Added Imports statement to DESCRIPTION; Rewrote README.Rmd to direct people to pkgdown generated web site; Configured pkgdown; Updated package level documentation; Fixed tab.width bug in fl.R; Added Roxygen directives to variables in reinstall_packages.r;

--- R/dgutils-package.R
+++ R/dgutils-package.R
@@ -1,55 +1,34 @@
-#' dgutils-package: Interactive and Analysis Script Utility Functions.
+#' Interactive and Analysis Script Utility Functions.
 #'
-#' The dgutils package provides three categories of R objects: aliases,
-#' functions, and graphical themes.
+#' This package consists of  aliases,
+#' functions, and graphical themes to improve your efficiency and workflow.
 #'
-#' These functions will improve your efficiency and R workflow
-#' and benefit from online documentation.
-#'
-#' After a year of using different mechanisms to establish a default set of packges
-#' when you start R, my conclusion is \code{option()} in \code{Rprofile.site} should
-#' not be used.  A base R environment consists of 7 packages in addition to
-#' \code{base}. The common idiom
-#' \code{option(defaultPackage = c(getOptions("defaultPackages"), "my-package",
-#' "your-package")} assumes that your R environment is only initialized once.
-#' That's what I thought until I started using the \code{devtools} package and
-#' discovered that your R session can be initialized by others.  Each initialization
-#' adds your packages to the R environment stack.
-#'
-#' Here are some better solutions:
-#' \enumerate{
-#'   \item At the top of each file, use \code{library()} to load and attach each package
-#'   the file needs.  This is similar to python's import statement.
-#'   \item After a package is installed, you can reference any function directly
-#'   with the \code{::} notation.
-#'   \item And if you are using lots of functions from a package, it might be
-#'   easier to attach the entire package.  In that case, make your code a package
-#'   and use the \code{@import} and \code{@export} directives to do this automatically
-#'   for you.
+#' @section Installation:
+#' Install this package:
+#' \preformatted{
+#' install.packages("devtools")
+#' devtools::install_github("dgabbe/dgutils")
 #' }
 #'
 #' @section Aliases:
 #' Create some bash equivalents in aliases.R
 #'
 #' @section Functions:
-#' Utility/convenience functions are defined in functions.R
+#' Utility/convenience functions are defined in their own R file named
+#' for the function.
 #'
 #' @section Graphical Themes:
-#' This section is where your common idioms for programming graphics belong.
+#' This section is graphics programming constructs.
 #' Since R has a number of different packages, there's a separate file for each
 #' one.  Only ggplot is populated.
 #'
 #' @section \emph{graphics-ggplot.R}:
 #' The ggplot2 package is somewhat unique from other packages with heavy use of
 #' the "+" continuation operator and the huge number of objects and functions
-#' needed to generate a graph. Using the \code{Imports} directive and \code{::}
-#' notation made \code{theme_dg} nearly inscrutable. Just take a look at the
-#' gglot2 Namespace file,
-#' \url{https://github.com/hadley/ggplot2/blob/master/NAMESPACE}. So yes, that is
-#' why this package depends on ggplot2.
+#' needed to generate a graph.
 #'
 #' @docType package
-#' @name dgutils
+#' @name dgutils-package
 NULL
 
 
--- R/reinstall_packages.r
+++ R/reinstall_packages.r
@@ -1,7 +1,22 @@
-installed_pkgs <- c() # List of packages already installed
-error_pkgs <- c() # List of packages that were not installed for some reason
-new_pkgs <- c() # List of packages this function installed
+#' List of packages already installed.
+#' @keywords internal
+installed_pkgs <- c()
 
+#' List of packages that were not installed for some reason.
+#' @keywords internal
+error_pkgs <- c()
+
+#' List of packages this function installed.
+#' @keywords internal
+new_pkgs <- c()
+
+#' Default set of packages.
+#'
+#' To see a tidy list of them, refer to Examples:
+#' @examples
+#' dgutils::fl(my_packages)
+#'
+#' @export
 my_packages <- list(
   "crayon",
   c("github", "dgabbe/dgutils"),
@@ -10,6 +25,7 @@ my_packages <- list(
   "ggplot2",
   "knitr",
   "lubridate",
+  c("github", "hadley/pkgdown"),
   "plotrix",
   "RColorBrewer",
   "readr",
@@ -25,50 +41,34 @@ my_packages <- list(
   "tidyr"
 )
 
-#' Installs your standard set of packages
+#' Installs your standard set of packages.
 #'
 #' This function is part programming exercise and part solution to R upgrades.
-#' Only \code{\link{install.packages}() pkgs} param to  is supported.  For \code{devtools::install_*}
+#' Only \code{\link{install.packages}() pkgs} param is supported.  For \code{devtools::install_*}
 #' functions, only the first parameter, usually repo, is supported.  If you need
 #' something more advanced, there are plenty of similar functions already written.
 #'
 #' Each R release has its own library, \code{/Library/Frameworks/R.framework/Versions/x.x/Resources/library}.
-#' To avoid populating the new R library, I added a library just for 3.x versions
+#' To avoid redoing the package installs into the new R library, I added this entry to \code{.Renviron}:
+#' \preformatted{
+#' R_LIBS_USER=~/Library/R/3.x/library/
+#' }
 #' and this function to make managing multiple computers eaiser.
+#'
 #'
 #' @param pkgs a list of packages to install. Packages installed
 #' with \code{devtools::install_*} functions are specified as a character
 #' vector of 2 strings.  The first string is the suffix to complete the install
 #' funciton name.  The second string is the path to the package.  If this param
-#' is not specified, the set is defaulted to my_packages.
+#' is not specified, the set is defaulted to \code{dgutils::my_packages}.
 #'
 #' @return TRUE
+#'
+#' @section Future Development:
+#' It's possible that \code{my_packages} will become an option.
 #' @export
 #'
 reinstall_packages <- function (pkgs = my_packages) {
-  # if (is.na(pkgs))
-  #   pkgs <- list(
-  #     "crayon",
-  #     c("github", "dgabbe/dgutils"),
-  #     "directlabels",
-  #     "dplyr",
-  #     "ggplot2",
-  #     "knitr",
-  #     "lubridate",
-  #     "plotrix",
-  #     "RColorBrewer",
-  #     "readr",
-  #     "readxl",
-  #     "reshape2",
-  #     "rmarkdown",
-  #     "roxygen2",
-  #     "rsconnect",
-  #     "rversions",
-  #     "scales",
-  #     "shiny",
-  #     "stringr",
-  #     "tidyr"
-  #   )
 
   #
   # Install first so install_*()'s are available.  Don't attach devtools!
@@ -86,13 +86,15 @@ reinstall_packages <- function (pkgs = my_packages) {
 
 #' Helper function that invokes the proper install function
 #'
-#' @param p character vector representing package name passed to \code{install.packages()}.
+#' @param p character vector representing package name passed to \code{\link{install.packages}}.
 #' If a list, the first element is the suffix to append to \code{devtools::install_}.
-#' The second element is the path to the path package.
+#' The second element is the path to the path package. Refer to \code{\link[devtools]{install}} for more info.
 #'
 #' @section Limitations:
+#' There are many params to the install functions that are not supported at this time.
 #'
 #' @return returns a named list if successful.  Otherwise FALSE.
+#' @keywords internal
 #'
 installer <- function(p) {
   if (is.null(p)) {
@@ -110,7 +112,7 @@ installer <- function(p) {
       assign("installed_pkgs", append(installed_pkgs, p), inherits = TRUE)
       return(list("installed" = p))
     } else {
-      if (is.null(install.packages(p, quiet = TRUE))) {
+      if (is.null(utils::install.packages(p, quiet = TRUE))) {
         assign("error_pkgs", append(error_pkgs, p), inherits = TRUE)
         return(list("error" = p))
       }

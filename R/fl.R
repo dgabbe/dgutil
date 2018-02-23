38c931046defb679e96a2c36455cae7be5461bec
David G <dgabbe@acm.org>
2018-02-23 12:12:53 -0500

    Attempt to confirm to Hadley's Roxygen commenting conventions; updates to default_package list; incremented version number.

--- R/fl.R
+++ R/fl.R
@@ -1,24 +1,24 @@
-#' An indented, wrapped list styled after a definition.
+#' An indented, wrapped list styled after a definition
 #'
-#' There are many formatting or pretty printing functions available,
-#' some of them quite elaborate.  Mine uses only functions in \code{base}.
-#' It seems that many programmers don't realize the power of \code{strwrap}
-#' and the \code{sep} parameter for \code{\link{cat}}.
+#' There are many formatting or pretty printing functions available, some of
+#' them quite elaborate.  Mine uses only functions in \code{base}. It seems that
+#' many programmers don't realize the power of \code{strwrap} and the \code{sep}
+#' parameter for \code{\link{cat}}.
 #'
-#' @param x the R object to display. \code{\link{toString}} must be able to evaluate it.
+#' @param x the R object to display. \code{\link{toString}} must be able to
+#'   evaluate it.
 #'
-#' @param label a string to describe the list.  The remaining lines are
-#' indented by the number of characters from catcatenating \code{indent_string} and
-#' `label`.
+#' @param label a string to describe the list.  The remaining lines are indented
+#'   by the number of characters from catcatenating \code{indent_string} and
+#'   `label`.
 #'
 #' @param indent_string the string to prefix the first line.  Defaults to
-#' \code{tab.width} option number of spaces.  If you wanted your
-#' list indented 4 spaces, pass a string of 4 spaces to this parameter.
+#'   \code{tab.width} option number of spaces.  If you wanted your list indented
+#'   4 spaces, pass a string of 4 spaces to this parameter.
 #'
-#' @section R Trivia:
-#' Turns out the \code{tab.width} option is not set by default in base R as
-#' I discovered when I tried to build the package.  Now explicitly test to see if the
-#' option is set.
+#' @section R Trivia: Turns out the \code{tab.width} option is not set by
+#'   default in base R as I discovered when I tried to build the package.  Now
+#'   explicitly test to see if the option is set.
 #'
 #' @examples
 #' fl(search(), label = "search():  ")
